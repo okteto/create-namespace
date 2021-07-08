90299970f929f11379ed8ac62c57243ec2650031
Javier López Barba <javier@okteto.com>
2021-07-08 12:51:33 +0200

    Add support for custom certificates
    Signed-off-by: Javier López Barba <javier@okteto.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,12 @@ if [ -z $namespace ]; then
   exit 1
 fi
 
+if [[ ! -z "$OKTETO_CA_CERT" ]]; then
+   echo "Custom certificate is provided"
+   echo "$OKTETO_CA_CERT" > /usr/local/share/ca-certificates/okteto_ca_cert
+   update-ca-certificates
+fi
+
 members=$2
 membersArg=""
 IFS=","
