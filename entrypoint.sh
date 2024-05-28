55fdabd35172901ee5d3cd0ff91271151b6766e1
Tere <teresa@okteto.com>
2024-05-28 15:21:22 +0200

    Add log level (#12)
    * add log-level input
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * fix entrypoint variable
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>
    
    * update error message for unsupported log level
    
    ---------
    
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,8 +27,24 @@ do
    membersArg="-m '$v' $membersArg"
 done
 
-echo running: okteto namespace create $namespace $membersArg
-eval okteto namespace create $namespace $membersArg
+log_level=$3
+if [ ! -z "$log_level" ]; then
+  if [ "$log_level" = "debug" ] || [ "$log_level" = "info" ] || [ "$log_level" = "warn" ] || [ "$log_level" = "error" ] ; then
+    log_level="--log-level ${log_level}"
+  else
+    echo "unsupported log-level ${log_level}, supported options are: debug, info, warn, error"
+    exit 1
+  fi
+fi
+
+# https://docs.github.com/en/actions/monitoring-and-troubleshooting-workflows/enabling-debug-logging
+# https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
+if [ "${RUNNER_DEBUG}" = "1" ]; then
+  log_level="--log-level debug"
+fi
+
+echo running: okteto namespace create $namespace $membersArg $log_level
+eval okteto namespace create $namespace $membersArg $log_level
 
 echo running: okteto kubeconfig
 eval okteto kubeconfig
